063d8de4d7f7ee70dcf2ae8587d76bb0cfc33ad4
Thomas Bell <thomasbell@weber.edu>
2020-12-01 20:52:31 -0700

    users

--- app.py
+++ app.py
@@ -44,15 +44,6 @@ class Movie(db.Model):
     director = db.relationship("Director", backref=db.backref("Movie", lazy=True))
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
-    def check_password(self, value):
-        return check_password_hash(self.password, value)
-
-
 db.create_all()
 
 
